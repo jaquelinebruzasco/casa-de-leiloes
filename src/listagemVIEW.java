525ceef3036b73920277c6a1b925ab0dcdd05abb
Jaqueline Bruzasco <jaquelinebruzasco@gmail.com>
2024-03-11 11:26:30 -0300

    Navegação no botão Consultar Vendas implementada

--- src/listagemVIEW.java
+++ src/listagemVIEW.java
@@ -149,8 +149,11 @@ public class listagemVIEW extends javax.swing.JFrame {
     }//GEN-LAST:event_btnVenderActionPerformed
 
     private void btnVendasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVendasActionPerformed
-        //vendasVIEW vendas = new vendasVIEW(); 
-        //vendas.setVisible(true);
+        ProdutosDAO produtosdao = new ProdutosDAO();
+        ArrayList<ProdutosDTO> produtosVendidos = produtosdao.listarProdutosVendidos();
+    
+        vendasVIEW vendasView = new vendasVIEW(produtosVendidos);
+        vendasView.setVisible(true);
     }//GEN-LAST:event_btnVendasActionPerformed
 
     private void btnVoltarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVoltarActionPerformed
